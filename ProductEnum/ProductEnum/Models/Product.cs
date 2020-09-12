74171cb96ba1351c33dc1c429db6f7136e549e37
Miguel Tapia Montecinos <a2019118017@estudiantes.upsa.edu.bo>
2020-09-12 11:57:48 -0400

    Product Creado

---
+++ ProductEnum/ProductEnum/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductEnum.Models
+{
+    public enum CategoryType
+    { 
+       Electronicos = 10,
+       Alimenticios = 20,
+       Moda = 30,
+       Belleza = 40,
+       Automotriz = 50,
+       Libros = 60
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        
+        [Display(Name = "Nombre del Producto")]
+        [Required(ErrorMessage = "Debe Ingresar El Nombre")]
+        public string Name { get; set; }
+        [Display(Name = "Categoría del producto")]
+        [Required(ErrorMessage = "Debe Ingresar La Categoría")]
+        public CategoryType Category { get; set; }
+    }
+}
